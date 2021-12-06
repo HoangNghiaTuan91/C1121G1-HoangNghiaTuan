2a214318b6ad6025e2b19f18240e45666e4ab2e1
hoangnghiatuan91 <tuanhn.91@gmail.com>
2021-12-06 08:16:10 +0700

    up bai ss7

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/SuDungToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt('Nhập chiều rộng');
+let width=parseInt(inputWidth);
+inputHeight=prompt('Nhập chiều cao');
+let height=parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích hình chữ nhật là: " + area);
